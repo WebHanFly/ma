219a844fbfb2a426ac780c28b76dbb83bce60158
xiaoma <670567137@qq.com>
2017-03-01 20:43:17 +0800

    sdaf

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var list = [
-    // {
-    // 	title:'吃饭睡觉',
-    // 	ischecked:false
-    // },
-    //  {
-    //  	title:'打豆豆',
-    //  	ischecked:false
-    //  }
+    {
+    	title:'吃饭睡觉',
+    	ischecked:false
+    },
+     {
+     	title:'打豆豆',
+     	ischecked:true
+     }
 ];
 new Vue({
     el:".main",
     data:{
     	list:list,
     	todo:"",
-    	edtorTodos:""
+    	edtorTodos:"",
+        beforetitle:""
     },
     methods:{
     	addToDo:function(ev){
@@ -31,6 +32,33 @@ new Vue({
     	},
     	edtortodo:function(todo){
     		this.edtorTodos = todo;
-    	}
-    }
+            //编辑这条任务的时候记录下来，方便在取消编辑是回到原来的值
+            this.beforetitle = todo.title;
+    	},
+        edtortodoed:function(todo){
+            this.edtorTodos = '';
+        },
+        canceltodo:function(todo){
+            todo.title = this.beforetitle;
+            //div显示出来
+            this.edtorTodos = '';
+        }
+    },
+        directives:{
+            "focus":{
+                update(el,binding){
+                    if(binding.value){
+                        el.focus();
+                    }
+                }
+            }
+        },
+        computed:{
+            nochecked:function(){
+               return this.list.filter(function(item){
+                   return !item.ischecked
+                    }).length
+            }
+        }
+    
 });
